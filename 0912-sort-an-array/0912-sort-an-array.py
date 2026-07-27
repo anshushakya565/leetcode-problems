01db82eb6b5fc7804a2bb7352cce1b7cb660e868
Anshu Shakya <anshushakya6505@gmail.com>
2026-07-27 15:20:41 +0530

    Time: 695 ms (32.01%), Space: 27 MB (65.19%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.py
+++ 0912-sort-an-array/0912-sort-an-array.py
@@ -1,4 +1,26 @@
 class Solution:
     def sortArray(self, nums: List[int]) -> List[int]:
-        nums.sort()
+        if len(nums) > 1:
+            m = len(nums) // 2
+            lo = self.sortArray(nums[:m])
+            hi = self.sortArray(nums[m:])
+            i, j, k = 0, 0, 0
+
+            while i < len(lo) and j < len(hi):
+                if lo[i] <= hi[j]:
+                    nums[k] = lo[i]
+                    i += 1
+                else:
+                    nums[k] = hi[j]
+                    j += 1
+                k += 1
+            while i < len(lo):
+                nums[k] = lo[i]
+                i += 1
+                k += 1
+            while j < len(hi):
+                nums[k] = hi[j]
+                j += 1
+                k += 1
+
         return nums

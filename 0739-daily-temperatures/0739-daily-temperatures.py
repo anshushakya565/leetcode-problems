5891262040d5f3b13babb27fa4e7da0bad5f6332
Anshu Shakya <anshushakya6505@gmail.com>
2026-07-30 15:29:27 +0530

    Time: 146 ms (7.11%), Space: 28.3 MB (61.04%) - LeetHub

---
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -0,0 +1,13 @@
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        ans = [0] * len(temperatures)
+        st = []
+        for i in range(len(temperatures) - 1, -1, -1):
+            while st != [] and temperatures[st[-1]] <= temperatures[i]:
+                st.pop()
+            if st == []:
+                ans[i] = 0
+            else:
+                ans[i] = st[-1] - i
+            st.append(i)
+        return ans

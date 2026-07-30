7983dcbbab6130179d3fb73c467caf5663237022
Anshu Shakya <anshushakya6505@gmail.com>
2026-07-30 11:42:37 +0530

    Time: 0 ms (100%), Space: 19.3 MB (28.15%) - LeetHub

---
+++ 3174-clear-digits/3174-clear-digits.py
@@ -0,0 +1,9 @@
+class Solution:
+    def clearDigits(self, s: str) -> str:
+        st = []
+        for i in s:
+            if i.isdigit():
+                st.pop()
+            else:
+                st.append(i)
+        return "".join(st)

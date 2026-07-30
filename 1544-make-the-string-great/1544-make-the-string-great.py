c28f2d30a6b0c810b9c3c60fbea59182a4f5a981
Anshu Shakya <anshushakya6505@gmail.com>
2026-07-30 12:16:02 +0530

    Time: 0 ms (100%), Space: 19.2 MB (87.52%) - LeetHub

--- 1544-make-the-string-great/1544-make-the-string-great.py
+++ 1544-make-the-string-great/1544-make-the-string-great.py
@@ -2,7 +2,7 @@ class Solution:
     def makeGood(self, s: str) -> str:
         st = []
         for i in s:
-            if st != [] and i != st[-1] and i.lower() == st[-1].lower():
+            if st  and i != st[-1] and i.lower() == st[-1].lower():
                 st.pop()
             else:
                 st.append(i)
